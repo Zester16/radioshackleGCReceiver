918f18541e73a77276e5244327094a14024227fc
Zester16 <88012803+Zester16@users.noreply.github.com>
2022-07-01 14:08:29 +0530

    Add files via upload

--- js/receiver.js
+++ js/receiver.js
@@ -2,5 +2,10 @@ const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 // Optimizing for smart displays
 //const touchControls = cast.framework.ui.Controls.getInstance();
+// let playerElement = document.getElementsByTagName("cast-media-player")[0];
+// playerElement.style.setProperty('--splash-image', 'url("http://some/other/image.png")');
+const musicTrack = cast.framework.messages.MusicTrackMediaMetadata()
+musicTrack.albumName = "Classic Radio"
+
 
 context.start();
