22d786ff297067767b902ed54584df1906341bb8
Zester16 <88012803+Zester16@users.noreply.github.com>
2022-07-01 15:36:14 +0530

    Add files via upload

--- js/receiver.js
+++ js/receiver.js
@@ -10,7 +10,8 @@ playerManager.setMessageInterceptor(
     const musicTrack = new cast.framework.messages.MusicTrackMediaMetadata()
     musicTrack.albumName = "Classic Radio"
     musicTrack.title = "Classic Radio"
-    musicTrack.images[0] = "https://www.klassikradio.de//_nuxt/img/72ebcae.png"
+    musicTrack.images[0] = new cast.framework.messages.Image("https://www.klassikradio.de//_nuxt/img/72ebcae.png")
+
     request.media.metadata = musicTrack
     return request
   })
